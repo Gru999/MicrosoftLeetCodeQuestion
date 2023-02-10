1ba358f94547c7cd7520e40884bb230ed3bbfd38
fbaun <fbaun@HIGHTOWER>
2023-02-10 06:42:44 +0100

    -

--- MicrosoftLeetCodeQuestion/Program.cs
+++ MicrosoftLeetCodeQuestion/Program.cs
@@ -1,41 +1,54 @@
 ﻿using System;
 
-int[] myArray = {1, 4, 6, 2, 1, 7, 4, 6, 2, 8 };
+int[] myArray = { 1, 4, 6, 2, 1, 7, 4, 6, 2, 8, 44, 90, 90, 44, 55, 55 };
 
 static int FirstUniqIterativ(int[] myArray) {
 
-    ////basis solution
+
+    ////Løsning 1: basis solution
     //int index = 0;
-    //for (int i = 0; i < myArray.Length; i++) {
+    //for (int i = 0; i < myArray.Length; i++)
+    //{
     //    int counter = 0;
-    //    for (int j = 0; j < myArray.Length; j++) {
-    //        if (myArray[i] == myArray[j]) {
+    //    for (int j = 0; j < myArray.Length; j++)
+    //    {
+    //        if (myArray[i] == myArray[j])
+    //        {
     //            counter++;
     //        }
-    //        if (counter.Equals(2)) {
+    //        if (counter.Equals(2))
+    //        {
     //            break;
     //        }
     //    }
-    //    if (counter == 1 /*&& result.Equals(int.MinValue)*/) {
+    //    if (counter == 1)
+    //    {
     //        index = Array.IndexOf(myArray, myArray[i]);
     //        return index;
     //    }
     //}
     //return -1;
 
-    ////XOR, reverse bit calc - needs fixing beyond 9
-    //int res = 0; Array.Sort(myArray);
-    //foreach (int i in myArray) {
-    //    res = res ^ i;
-    //    if (res != 0 && res != i) {
-    //        return 15 - i;
-    //    }
-    //    //if (res != 0 && res != i) {
-    //    //    res = ~i; //could use &= for bitwise subtraction
-    //    //    return res = Array.IndexOf(myArray, res);
-    //    //} 
-    //}
-    //return res;
+    //Løsning 2: XOR, reverse bit calc - needs fixing beyond 9
+    int res = 0; Array.Sort(myArray);
+    foreach (int i in myArray)
+    {
+        res = res ^ i;
+        if (res != 0 && res != i)
+        {
+            res = res ^ i;
+            return res = Array.IndexOf(myArray, res);
+            //int bit = 15 - i;
+            //res = bit;
+            //break;
+            //return i - 1;
+        }
+        //if (res != 0 && res != i) {
+        //    res = ~i; //could use &= for bitwise subtraction
+        //    return res = Array.IndexOf(myArray, res);
+        //} 
+    }
+    return res;
 }
 
 //Console.WriteLine(FirstUniqIterativ(myArray));
