446726b42eb1d6b1e5ad836192799a6a031faee7
fbaun <fbaun@HIGHTOWER>
2023-02-10 06:44:11 +0100

    -

--- MicrosoftLeetCodeQuestion/Program.cs
+++ MicrosoftLeetCodeQuestion/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 
-int[] myArray = { 1, 4, 6, 2, 1, 7, 4, 6, 2, 8, 44, 90, 90, 44, 55, 55 };
+int[] myArray = { 1, 4, 6, 2, 1, 7, 4, 6, 2, 8 };
 
 static int FirstUniqIterativ(int[] myArray) {
-
 
     ////Løsning 1: basis solution
     //int index = 0;
@@ -27,30 +26,22 @@ static int FirstUniqIterativ(int[] myArray) {
     //        return index;
     //    }
     //}
-    //return -1;
+    //return index;
 
-    //Løsning 2: XOR, reverse bit calc - needs fixing beyond 9
-    int res = 0; Array.Sort(myArray);
-    foreach (int i in myArray)
-    {
-        res = res ^ i;
-        if (res != 0 && res != i)
-        {
-            res = res ^ i;
-            return res = Array.IndexOf(myArray, res);
-            //int bit = 15 - i;
-            //res = bit;
-            //break;
-            //return i - 1;
-        }
-        //if (res != 0 && res != i) {
-        //    res = ~i; //could use &= for bitwise subtraction
-        //    return res = Array.IndexOf(myArray, res);
-        //} 
-    }
-    return res;
+
+    ////Løsning 2: Bitwise XOR implementering
+    //int res = 0; Array.Sort(myArray);
+    //foreach (int i in myArray)
+    //{
+    //    res = res ^ i;
+    //    if (res != 0 && res != i)
+    //    {
+    //        res = res ^ i;
+    //        return res = Array.IndexOf(myArray, res);
+    //    }
+    //}
+    //return res;
 }
 
-//Console.WriteLine(FirstUniqIterativ(myArray));
 Console.WriteLine(myArray[FirstUniqIterativ(myArray)]);
 Console.ReadLine();
